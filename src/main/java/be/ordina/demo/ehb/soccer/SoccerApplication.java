2f6d5bff90b30fd2f6ec5b91479efa442cadc24c
Tom Verelst <tom.verelst@ordina.be>
2018-01-30 10:36:34 +0100

    Initial import of generated project

---
+++ src/main/java/be/ordina/demo/ehb/soccer/SoccerApplication.java
@@ -0,0 +1,12 @@
+package be.ordina.demo.ehb.soccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerApplication.class, args);
+	}
+}
